1bb7cea68b74d054466d119b8153576ee908b7cf
nicolesikorski <nsikorskin@gmail.com>
2022-06-07 19:02:11 -0300

    consigna 1 header y footer

--- js/resultado-busqueda.js
+++ js/resultado-busqueda.js
@@ -1,1 +1,8 @@
-
+//1.Header y footer: el campo no debe estar vacio
+function validateForm() {
+    let resultadoBusqueda = document.forms["myForm"]["fname"].value;
+    if (resultadoBusqueda == "" ) {
+      alert("El campo esta vacío. Debe rellenar el campo");
+      return false;
+    }
+  }
