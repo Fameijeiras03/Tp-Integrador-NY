22baf586728d13564e7912a41ea5d0edd09b68f7
Fameijeiras03 <franciscoameijeiras03@gmail.com>
2022-06-10 15:47:59 -0300

    for

---
+++ js/home.js
@@ -0,0 +1,55 @@
+let proxy = 'https://api.allorigins.win/raw?url='
+let endpoint =  'https://api.deezer.com/chart'
+let url = proxy+endpoint
+
+let elementos = ''
+
+fetch(url).then(function (response) {
+    return response.json()
+    //agarro la info en json
+})
+.then(function (dataTr) {
+    console.log(dataTr);
+    let info = dataTr.tracks.data
+    
+    let etiquetaElementos = document.querySelector('#contenedor')
+
+    for (let i = 0; i < 5; i++) {
+
+        etiquetaElementos.innerHTML +=
+
+        `
+      
+        <article class="">
+        <img class="imgalbum" src=${info[i].album.cover}>
+        <h3 class="tamanofuenteh3">${info[i].title}</h3>
+
+
+    
+        </article>
+       
+        
+        `
+        
+        
+    }
+
+})
+.catch(function (error) {
+    console.log(error);
+})
+
+
+
+
+
+
+/* <a href="./detallecancion.html" class="aimg">
+                    <img src="./img/timelezz.jpeg" alt="logo" class="imgalbum">
+                </a>
+                <h3 class="tamanofuenteh3">
+                    Dilema
+                </h3>
+                <p class="tamanofuentetexto">
+                    <a href="./detalle-artista.html" class="estiloa">Jhay Cortez</a>
+                </p> */
