e6a48f008ff37bf14a9b47b68a304b368b37c6de
Fameijeiras03 <franciscoameijeiras03@gmail.com>
2022-06-11 13:56:50 -0300

    arrgelos js, section del album hecho

--- js/home.js
+++ js/home.js
@@ -6,14 +6,17 @@ let url = proxy+endpoint
 
 let elementos = ''
 
-fetch(url).then(function (response) {
+fetch(url)
+
+        .then(function (response) {
     return response.json()
     //agarro la info en json
 })
-.then(function (dataTr) {
-    console.log(dataTr);
-    let info = dataTr.tracks.data
-    
+
+        .then(function (dataTr) {
+             console.log(dataTr);
+            let info = dataTr.tracks.data
+// Primer article ------ Canciones//
     let etiquetaElementos = document.querySelector('#contenedor')
 
     for (let i = 0; i < 5; i++) {
@@ -23,14 +26,19 @@ fetch(url).then(function (response) {
         `
        <article class="">
        <a href="./detalle-album.html"><img class="imgalbum" src=${info[i].album.cover}></a>
+       <a href="./detalle-cancion.html"><p class="tamanofuenteh3">${info[i].title}</p></a>
        <a href="./detalle-artista.html"><h3 class="tamanofuenteh3">${info[i].artist.name}</h3></a>
-       <a href="./detallecancion.html"> <p class="tamanofuenteh3">${info[i].title}</p></a>
+     
        
         
         </article>
         
         `
+
     }
+
+// Segundo article ------ Artista//
+
     etiquetaElementos = document.querySelector('#contenedor2')
     for (let i = 5; i < 10; i++) {
 
@@ -38,43 +46,43 @@ fetch(url).then(function (response) {
 
         `
        <article class="">
-       <a href="./detalle-album.html"><img class="imgalbum" src=${info[i].album.cover}></a>
+       <a href="./detalle-artista.html"><img class="imgalbum" src=${info[i].artist.picture_small}></a>
        <a href="./detalle-artista.html"><h3 class="tamanofuenteh3">${info[i].artist.name}</h3></a>
-       <a href="./detallecancion.html"> <p class="tamanofuenteh3">${info[i].title}</p></a>
-        </article>
-        
+        </article> 
         `
     }
-    /*etiquetaElementos = document.querySelector('#contenedor3')
-    for (let i = 10; i < 15; i++) {
 
-        etiquetaElementos.innerHTML +=
+// Tercer article ------ album//
+    
+etiquetaElementos = document.querySelector('#contenedor3')
+for (let i = 0; i < 5; i++) {
 
-        `
-       <article class="">
-        <img class="imgalbum" src=${info[i].album.cover}>
-        <h3 class="tamanofuenteh3">${info[i].title}</h3>
-        </article>
-        
-        `
-    }*/
+    etiquetaElementos.innerHTML +=
+
+    `
+   <article class="">
+   <a href="./detalle-album.html"><img class="imgalbum" src=${info[i].album.cover}></a>
+   <a href="./detalle-cancion.html"><p class="tamanofuenteh3">${info[i].album.title}</p></a>
+   <a href="./detalle-artista.html"><h3 class="tamanofuenteh3">${info[i].artist.name}</h3></a>
+ 
+   
+    
+    </article>
+    
+    `
+
+}
+
+
+
 })
 .catch(function (error) {
     console.log(error);
-})
+})  
+
+   
 
 
 
 
-
-
-/* <a href="./detallecancion.html" class="aimg">
-                    <img src="./img/timelezz.jpeg" alt="logo" class="imgalbum">
-                </a>
-                <h3 class="tamanofuenteh3">
-                    Dilema
-                </h3>
-                <p class="tamanofuentetexto">
-                    <a href="./detalle-artista.html" class="estiloa">Jhay Cortez</a>
-                </p> */
 
