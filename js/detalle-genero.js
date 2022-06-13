42c6b8e9da9f2a25449507e4e926a12663ea2193
nicolesikorski <nsikorskin@gmail.com>
2022-06-13 20:12:21 -0300

    cambios

--- js/detalle-genero.js
+++ js/detalle-genero.js
@@ -1,15 +1,16 @@
 
 let qs = location.search;
-let querytoObjetoLiteral = new URLSearchParams(qs) 
+let qstoObjetoLiteral = new URLSearchParams(qs) //para convertir una querystring a un objeto literal
+let id = qstoObjetoLiteral.get('id');
 
-//para obtener la propiedad llamada id
-let id = querytoObjetoLiteral.get('id');
 
 //para buscar el genero mi API tenia un link que trae todos los generos, con / y el id del genero, busca el genero
 
 let proxy = 'https://api.allorigins.win/raw?url='
-let endpoint =  'https://api.deezer.com/genre'
-let url = proxy+endpoint
+let endpoint =  `https://api.deezer.com/genre/${id}`
+const url = proxy+endpoint;
+
+
 
 //para buscar la informacion, uso fetch
 fetch(url)
@@ -19,9 +20,9 @@ fetch(url)
     .then(function(data) {   
         let info = data.data
     //quiero capturar el titulo, imagen,etc
-      let title = document.querySelector('#h3detalledegenero');
-      let image = document.querySelector('#imgdetallegenero')
-      let detalle = document.querySelector('#parrafodetallegenero')
+    let title = document.querySelector('#h3detalledegenero');
+    let image = document.querySelector('#imgdetallegenero')
+    let detalle = document.querySelector('#parrafodetallegenero')
 
         //para insertarle los valores que obtenga con estas propiedades a estos elementos del dom
 
@@ -33,7 +34,3 @@ fetch(url)
   .catch(function(errores) {
       console.log(errores);
   })
-  
-
-
- 
