3b28d91fa04a1741021f0e6769c1cc1754a1e92d
nicolesikorski <nsikorskin@gmail.com>
2022-06-14 14:25:58 -0300

    commit

--- js/detalle-cancion.js
+++ js/detalle-cancion.js
@@ -34,3 +34,24 @@
 //         })
 //         .catch(function(error) {
 //         }) 
+
+/*
+let qs = location.search;
+let qstoObjetoLiteral = new URLSearchParams(qs) //para convertir una querystring a un objeto literal
+let id = qstoObjetoLiteral.get('id');
+
+let proxy = 'https://api.allorigins.win/raw?url=    '
+let endpoint =  `https://api.deezer.com/chart?${id}`
+let url = proxy+endpoint
+
+fetch(url)
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+    for (let i = 0; i < data.length; i++) {
+        if (id== data[i].id){
+          document.querySelector('#divcanciones').innerHTML = `<article>${data[i].title}</article>`
+        }
+    }})
+    */
