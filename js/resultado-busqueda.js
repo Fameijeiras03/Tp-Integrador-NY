2f96a3aaa504bc59bca9056407f4c22fb729a85e
nicolesikorski <nsikorskin@gmail.com>
2022-06-14 20:32:12 -0300

    resultado busqueda

---
+++ js/resultado-busqueda.js
@@ -0,0 +1,33 @@
+
+let proxy = 'https://api.allorigins.win/raw?url=' ; 
+let endpoint =  'https://api.deezer.com/artist/' ;
+let urlArtist = proxy+endpoint;
+
+
+function searchArtist() {
+    
+    let datoFormulario = document.getElementById('busqueda').value //con el .value toma el dato que escribi en el formulario (para el valor que escribiste ene l formulario)
+    let ArtistaBuscar = `${urlArtist}${datoFormulario}` 
+    
+fetch(ArtistaBuscar)
+        .then(function (response) {
+    return response.json() ;
+   
+})
+        .then(function (data) {
+         
+})
+.catch(function (error) {
+    alert(error);
+})
+
+}
+
+
+
+
+
+
+
+
+
