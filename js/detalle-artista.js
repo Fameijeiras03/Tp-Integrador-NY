64779fa2d36a811ac0a98d35e2b8a50b89dbd3da
nicolesikorski <nsikorskin@gmail.com>
2022-06-15 10:32:59 -0300

    home arreglado

--- js/detalle-artista.js
+++ js/detalle-artista.js
@@ -3,7 +3,33 @@ let queryString = location.search;
 let queryStringObjLiteral = new URLSearchParams(queryString);
 let id = queryStringObjLiteral.get('id');
 
-let urlArtistas = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id} `
+let urlArtistas = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id}`
 let urlCancionesDelArtistas = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id}/top?limit=5`
+let seccionDetalleArtista = document.querySelector('#seccionDetalleArtista')
+
 
 fetch(urlArtistas)
+.then(function (response) {
+    return response.json(); 
+})
+.then(function(data) {   
+   console.log(data);
+    
+   seccionDetalleArtista.innerHTML += `<article>
+        <h1 class="tamanofuenteh1"> El artista es: ${data.name}</h1>
+        <img src="" alt="${data.album.cover}" class="imgalbum">
+        <h2 class="tamanofuenteh2">Top 5 Canciones</h2>
+        <ul class="datos estilo-listas listadetalles">
+            <li>Nombre: </li>
+        </ul>
+
+    </article>
+    
+    `
+    
+
+})                   
+
+.catch(function(errores) {
+  console.log(errores);
+})
