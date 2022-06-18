8293df7c642666954662e90cfdc0db113458cac4
nicolesikorski <nsikorskin@gmail.com>
2022-06-18 12:09:59 -0300

    result

--- js/home.js
+++ js/home.js
@@ -1,8 +1,5 @@
 //!no sacar este espacio que hay, no se pq si lo sacamos se deja de renderizar todo
-
-let proxy = 'https://api.allorigins.win/raw?url=    '
-let endpoint =  'https://api.deezer.com/chart'
-let url = endpoint
+let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart'
 
 
 let elementos = ''
@@ -15,15 +12,9 @@ fetch(url)
 })
 
         .then(function (dataTr) {
-             console.log(dataTr);
-
-            let infoTracks = dataTr.tracks.data
-
-            let infoAlbums = dataTr.albums.data
-
-            let infoArtist = dataTr.artists.data
-
-        
+             //console.log(dataTr);
+            let info = dataTr.tracks.data
+            //console.log(info);
             
 // Primer article ------ Canciones//
     let etiquetaElementos = document.querySelector('#contenedor')
@@ -34,9 +25,9 @@ fetch(url)
 
         `
        <article class="">
-       <a href="./detalle-cancion.html?id=${infoTracks[i].id}"><img class="imgalbum" src=${infoTracks[i].album.cover_medium}></a>
-       <a href="./detalle-cancion.html?id=${infoTracks[i].id}"><p class="tamanofuenteh3">${infoTracks[i].title}</p></a>
-       <a href="./detalle-artista.html?id=${infoTracks[i].id}"><h3 class="tamanofuenteh3">${infoTracks[i].artist.name}</h3></a>
+       <a href="./detalle-cancion.html?id=${info[i].id}"><img class="imgalbum" src=${info[i].album.cover_medium}></a>
+       <a href="./detalle-cancion.html?id=${info[i].id}"><p class="tamanofuenteh3">${info[i].title}</p></a>
+       <a href="./detalle-artista.html?id=${info[i].artist.id}"><h3 class="tamanofuenteh3">${info[i].artist.name}</h3></a>
      
        
         
@@ -55,26 +46,41 @@ fetch(url)
 
         `
        <article class="">
-       <a href="./detalle-artista.html?id=${infoArtist[i].id}"><img class="imgalbum" src=${infoArtist[i].picture_medium}></a>
-       <a href="./detalle-artista.html?id=${infoArtist[i].id}"><h3 class="tamanofuenteh3">${infoArtist[i].name}</h3></a>
+       <a href="./detalle-artista.html?id=${info[i].artist.id}"><img class="imgalbum" src=${info[i].artist.picture_medium}></a>
+       <a href="./detalle-artista.html?id=${info[i].artist.id}"><h3 class="tamanofuenteh3">${info[i].artist.name}</h3></a>
         </article> 
         `
     }
 
 
 
+})
+.catch(function (error) {
+    console.log(error);
+})  
+
 // Tercer article ------ album//
-    
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums')
+
+.then(function (response) {
+    return response.json(); 
+})
+
+.then(function(data) {   
+console.log(data);
+let primerosAlbumes = data.data.slice(0,6)  //agarro la informacion del data y elijo de que elemento a que elemento quiero usar
+
 etiquetaElementos = document.querySelector('#contenedor3')
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < primerosAlbumes.length-1; i++) {
 
     etiquetaElementos.innerHTML +=
 
     `
    <article class="">
-   <a href="./detalle-album.html?id=${infoAlbums[i].id}"><img class="imgalbum" src=${infoAlbums[i].cover_medium}></a>
-   <a href="./detalle-album.html?id=${infoAlbums[i].id}"><p class="tamanofuenteh3">${infoAlbums[i].title}</p></a>
-   <a href="./detalle-artista.html?id=${infoAlbums[i].id}"><h3 class="tamanofuenteh3">${infoAlbums[i].artist.name}</h3></a>
+
+   <a href="./detalle-album.html?id=${primerosAlbumes[i].id}"><img class="imgalbum" src=${primerosAlbumes[i].cover_medium}></a>
+   <a href="./detalle-album.html?id=${primerosAlbumes[i].id}"><p class="tamanofuenteh3">${primerosAlbumes[i].title}</p></a>
+   <a href="./detalle-artista.html?id=${primerosAlbumes[i].artist.id}"><h3 class="tamanofuenteh3">${primerosAlbumes[i].artist.name}</h3></a>
  
    
     
@@ -84,11 +90,7 @@ for (let i = 0; i < 5; i++) {
 }
 
 
-
 })
-.catch(function (error) {
-    console.log(error);
-})  
 
 
 
