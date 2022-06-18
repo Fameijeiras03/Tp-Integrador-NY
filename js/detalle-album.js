434dfeebebe3cce717f71ab2d39e03fe63220ce4
nicolesikorski <nsikorskin@gmail.com>
2022-06-18 12:19:27 -0300

    colores li

--- js/detalle-album.js
+++ js/detalle-album.js
@@ -55,7 +55,7 @@ fetch(url)
     for (let i = 0; i < canciones.length - 1; i++) {
       //pongo el -1 para que me tome todas las canciones
 
-      contenidoDeLista += `<li> <p style="color: white;"> ${canciones[i].title}</p></li>`;
+      contenidoDeLista += `<li> ${canciones[i].title} </li>`;
      
     }
 
