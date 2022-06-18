6035df5ff7656ab3d53823a01036677f3c36ec94
nicolesikorski <nsikorskin@gmail.com>
2022-06-18 12:04:01 -0300

    pruebas en result busqueda

--- js/resultado-busqueda.js
+++ js/resultado-busqueda.js
@@ -1,4 +1,6 @@
 
+
+
 let urlArtist ='https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/'
 
 
@@ -6,9 +8,10 @@ function searchArtist() {
     
     let sectionResultado = document.querySelector('#resultadoBusquedaSection')
 
-    let datoFormulario = document.querySelector('#busqueda').value //con el .value toma el dato que escribi en el formulario (para el valor que escribiste ene l formulario)
+    let datoFormulario = document.querySelector('#busqueda').value //con el .value toma el dato que escribi en el formulario (para el valor que escribiste en el formulario)
     let ArtistaBuscar = `${urlArtist}${datoFormulario}` 
-    
+   
+
 fetch(ArtistaBuscar)
         .then(function (response) {
             console.log(response); //imprime el estado de la promesa (estados de codigo), si el estado de la peticion es 200, que haga algo, sino que muestre el spinner, porque quiere decir que no se cargo
@@ -28,7 +31,7 @@ fetch(ArtistaBuscar)
             console.log(data);
 
         if (data.name) {
-            sectionResultado.innerHTML =      //no le pongo += asi no me va sumando los distintos resultados de busqueda que hago
+            sectionResultado.innerHTML =      //no le pongo += asi no me va sumando los distintos resultados de busqueda que hago y cada vez que pongo uno nuevo se va el anterior
             `
             <article>
             <h2 class="tamanofuenteh3">Tu resultado de busqueda...</h2>
@@ -47,14 +50,4 @@ fetch(ArtistaBuscar)
 })
 
 }
-/*
-function fetchAlbumArtist(artist) {
-    let urlAlbum = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/'
-    fetch(urlAlbum)
-}
-*/
-
-
-
-
 
