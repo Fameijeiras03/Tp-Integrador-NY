f18b6bea6637fddd022d3c93deca1c2afd7da08c
Fameijeiras03 <franciscoameijeiras03@gmail.com>
2022-06-20 20:20:02 -0300

    textos

--- js/detalle-cancion.js
+++ js/detalle-cancion.js
@@ -6,14 +6,16 @@ let id = qstoObjetoLiteral.get('id');
 
 console.log(id);
 
-
+// declaro la api y la guardo dentro de una variable, donde la info que quiero traer es el id
 
 let url = ` https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${id}`
 console.log(url);
-
+// fetch para poder buscar esa info
 fetch(url)
     .then(function (response) {
         return response.json()
+
+        // transformo la info de json a un lenguaje que lo puede leer el js,
     })
     .then(function (data) {
         console.log(data)
@@ -29,7 +31,7 @@ fetch(url)
          nombreCancion.innerText = 'Canción:' + ' ' + data.title
          nombreArtista.innerText = 'Artista:' + ' ' + data.artist.name
          nombreDisco.innerText = 'Albúm:'+ ' ' + data.album.title 
-         player.src
+         player.src = data.preview
          
         
 
@@ -73,7 +75,7 @@ if (miPlaylist.includes(id)) {
 
 let indice = miPlaylist.indexOf(id)
 miPlaylist.splice(indice,1)
-fav.innerText = "Agregar a miPlaylist"
+fav.innerText = "Agregar a mi Playlist"
 
 } else {
 
